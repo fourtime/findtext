5ae4dce6e969d8d9f63a68bb0ed47a311564adec
Dirceu Belém <dirceu@fourtime.com>
2022-02-17 16:20:30 -0300

    Espaço

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -86,7 +86,7 @@ public class Main {
                             System.out.println(path);
                             shownFile = true;
                         }
-                        System.out.println(l + ":" + linha);
+                        System.out.println(l + ": " + linha);
                         total++;
                     }
                     if (linha == null) {
