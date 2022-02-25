b8302ff0d2d892f5901271fbe97c77f44ab88f3b
Dirceu Belém <dirceu@fourtime.com>
2022-02-25 12:05:43 -0300

    Ajustando alteração

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,7 +3,6 @@ package app;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
@@ -11,13 +10,17 @@ public class Main {
     private static String text;
     private static String extension;
     private static int total = 0;
+    private static String changeTo = null;
 
     public static void main(String[] args) {
         try {
             extension = EXTENSION;
             text = args[1];
-            if (args.length == 3) {
+            if (args.length >= 3) {
                 extension = args[2];
+            }
+            if (args.length >= 4) {
+                changeTo = args[3];
             }
             System.out.println("Directory: " + args[0]);
             System.out.println("Find: " + text);
@@ -28,6 +31,7 @@ public class Main {
         } catch (Exception e) {
             System.out.println("java -jar findtext.jar PATH TEXT");
             System.out.println("java -jar findtext.jar PATH TEXT EXTENSIONS(php,txt,java,json,xls,xlsx,log)");
+            System.out.println("java -jar findtext.jar PATH TEXT EXTENSIONS(php,txt,java,json,xls,xlsx,log) CHANGETO");
         }
     }
 
@@ -47,6 +51,7 @@ public class Main {
                         int q = openFile(f.getAbsolutePath());
                         if (q > 0) {
                             System.out.println("Total:" + q);
+                            modifyFile(f, text, changeTo);
                         }
                     }
                 }
@@ -102,14 +107,39 @@ public class Main {
         return qtd;
     }
 
-    public static void escritor(String path) throws IOException {
-        BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
-        String linha = "";
-        Scanner in = new Scanner(System.in);
-        System.out.println("Escreva algo: ");
-        linha = in.nextLine();
-        buffWrite.append(linha + "\n");
-        buffWrite.close();
+    public static String readFile(File file) throws IOException {
+        int len = (int) file.length();
+        byte[] bytes = new byte[len];
+        try (FileInputStream fis = new FileInputStream(file)) {
+            assert len == fis.read(bytes);
+        }
+        return new String(bytes, "UTF-8");
+    }
+
+    private static void modifyFileOld(File f, String oldString, String newString) throws Exception {
+        String content = readFile(f);
+        String newContent = content.replaceAll(oldString, newString);
+        try (FileWriter writer = new FileWriter(f)) {
+            writer.write(newContent);
+        }
+    }
+
+    private static void modifyFile(File file, String oldString, String newString) {
+        StringBuilder oldContent = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = reader.readLine();
+            while (line != null) {
+                oldContent.append(line).append(System.lineSeparator());
+                line = reader.readLine();
+            }
+            String content = oldContent.toString();
+            String newContent = content.replaceAll(oldString, newString);
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write(newContent);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
